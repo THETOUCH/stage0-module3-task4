a8a09904240febba188f168440d7518fe95db108
THETOUCH <super.ilgiznex@gmail.com>
2023-10-18 17:06:06 +0600

    123

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,14 +2,6 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-//        int c = (int)'c';
-//        int h = (int)'h';
-//        int a = (int)'a';
-//        int r = (int)'r';
-//        System.out.println(c);
-//        System.out.println(h);
-//        System.out.println(a);
-//        System.out.println(r);
         int cc = 99;
         int hh = 104;
         int aa = 97;
